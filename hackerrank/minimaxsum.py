c1224dab5d8d1dcc5d480b1ebb1721ba39c263cb
itsanti <kurov.aleksandr+github@gmail.com>
2021-12-01 23:04:58 +0300

    3 month: mini-max sum

---
+++ hackerrank/minimaxsum.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    #arr.sort()
+    #print(sum(arr[:-1]), sum(arr[1:]))
+    print(sum(arr) - max(arr), sum(arr) - min(arr))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
+
